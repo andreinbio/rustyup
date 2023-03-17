4ac012f9c998928bea1bcda31d50108414992332
Andrei Nagacevschi <andrein_bio@yahoo.com>
2023-03-17 18:26:33 +0200

    Added initial commit with slint integration.

---
+++ src/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    slint_build::compile("ui/start_page.slint").unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
+
+slint::include_modules!();
+
 fn main() {
-    println!("Hello, world!");
+    let main_window = MainWindow::new();
+    main_window.set_custom_min_height(400.0);
+    let window = main_window.window();
+    // window.on_close_requested(|| {
+    //     dbg!("test");
+    // });
+    main_window.run();
 }
